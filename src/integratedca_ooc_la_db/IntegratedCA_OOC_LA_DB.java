fab7113405c5e0c057ffc2c258c63ce625171ff4
Vladimyr Stefansky <2021417@student.cct.ie>
2022-12-04 16:31:12 +0000

    Start of the project.

---
+++ src/integratedca_ooc_la_db/IntegratedCA_OOC_LA_DB.java
@@ -0,0 +1,12 @@
+
+package integratedca_ooc_la_db;
+
+import java.util.Scanner;
+
+    public class IntegratedCA_OOC_LA_DB {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
